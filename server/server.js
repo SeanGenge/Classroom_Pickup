426a6ebf419c519c613948857814b0d464d637a2
Sean Genge <seangenge@gmail.com>
2023-01-14 21:49:03 +1100

    Fixed production line code

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/public')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // turn on connection to db and server
