c57b84f64299edf19e6d4a31095d790663336367
Sean Genge <seangenge@gmail.com>
2023-01-14 21:41:45 +1100

    Serving the index file

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log('Now listening on http://localhost:3000'));
